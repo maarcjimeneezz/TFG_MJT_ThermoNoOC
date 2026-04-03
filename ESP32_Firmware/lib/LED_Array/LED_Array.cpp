359cc109bda585756b520c62b78d633e2fa1bd84
maarcjimeneezz <mjimento12@alumnes.ub.edu>
2026-04-03 20:34:28 +0200

    changes to have constant names according to LED_Array.h file

--- ESP32_Firmware/lib/LED_Array/LED_Array.cpp
+++ ESP32_Firmware/lib/LED_Array/LED_Array.cpp
@@ -5,16 +5,16 @@ LED_Array::LED_Array() {}
 void LED_Array::begin()
 {
     // Configure LEDC PWM functionalities
-    ledcSetup(ch1, freq, resolution);
-    ledcSetup(ch2, freq, resolution);
-    ledcSetup(ch3, freq, resolution);
-    ledcSetup(ch4, freq, resolution);
+    ledcSetup(ledch1, ledFreq, ledRes);
+    ledcSetup(ledch2, ledFreq, ledRes);
+    ledcSetup(ledch3, ledFreq, ledRes);
+    ledcSetup(ledch4, ledFreq, ledRes);
 
     // Bind the physical GPIO pins (from Pinout.h) to the PWM channels
-    ledcAttachPin(PIN_C1_LEDS, ch1);
-    ledcAttachPin(PIN_C2_LEDS, ch2);
-    ledcAttachPin(PIN_C3_LEDS, ch3);
-    ledcAttachPin(PIN_C4_LEDS, ch4);
+    ledcAttachPin(PIN_C1_LEDS, ledch1);
+    ledcAttachPin(PIN_C2_LEDS, ledch2);
+    ledcAttachPin(PIN_C3_LEDS, ledch3);
+    ledcAttachPin(PIN_C4_LEDS, ledch4);
 
     // Initialize all LEDs to 0 (OFF)
     allOff();
@@ -28,16 +28,16 @@ void LED_Array::setBrightness(int group, int value)
     switch (group)
     {
     case 1:
-        ledcWrite(ch1, dutyCycle);
+        ledcWrite(ledch1, dutyCycle);
         break;
     case 2:
-        ledcWrite(ch2, dutyCycle);
+        ledcWrite(ledch2, dutyCycle);
         break;
     case 3:
-        ledcWrite(ch3, dutyCycle);
+        ledcWrite(ledch3, dutyCycle);
         break;
     case 4:
-        ledcWrite(ch4, dutyCycle);
+        ledcWrite(ledch4, dutyCycle);
         break;
     default:
         // Optional: Handle invalid group numbers
@@ -47,8 +47,8 @@ void LED_Array::setBrightness(int group, int value)
 
 void LED_Array::allOff()
 {
-    ledcWrite(ch1, 0);
-    ledcWrite(ch2, 0);
-    ledcWrite(ch3, 0);
-    ledcWrite(ch4, 0);
+    ledcWrite(ledch1, 0);
+    ledcWrite(ledch2, 0);
+    ledcWrite(ledch3, 0);
+    ledcWrite(ledch4, 0);
 }
